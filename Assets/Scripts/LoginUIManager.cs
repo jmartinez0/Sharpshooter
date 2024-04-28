99ae8bbb6eb36912fee9ef7bc2a4d36994f9438a
jmartinez0 <jmartinez.16c@gmail.com>
2024-04-28 01:33:42 -0400

    Log in functionality complete

--- Assets/Scripts/LoginUIManager.cs
+++ Assets/Scripts/LoginUIManager.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class LoginUIManager : MonoBehaviour
 {
-
+    public static LoginUIManager instance;
     public GameObject login;
     public GameObject register;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
